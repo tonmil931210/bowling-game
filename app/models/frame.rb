e204b2c51b3cc2e54d24eedfb8a0bccf462778fb
tonmil931210 <milton931210@gmail.com>
2018-08-27 21:02:06 +0000

    fix cases

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -3,8 +3,8 @@ class Frame < ApplicationRecord
   
   validates_presence_of :try1, :try2, :frame_by_user, :turn
   
-  validates :try1, :numericality => { :only_integer => true, :less_than_or_equal_to => 10,  :greater_than_or_equal_to => 0 }, :allow_blank => false
-  validates :try2, :numericality => { :only_integer => true, :less_than_or_equal_to => 10, :greater_than_or_equal_to => 0}, :allow_blank => false
+  validates :try1, :numericality => { :only_integer => true, :less_than_or_equal_to => 10,  :greater_than_or_equal_to => 0 }
+  validates :try2, :numericality => { :only_integer => true, :less_than_or_equal_to => 10, :greater_than_or_equal_to => 0}
   validates :try3, :numericality => { :only_integer => true, :less_than_or_equal_to => 10, :greater_than_or_equal_to => 0}, :allow_blank => true
   validates :turn, :numericality => { :only_integer => true, :greater_than => 0, :less_than_or_equal_to => 10}
   
@@ -29,23 +29,28 @@ class Frame < ApplicationRecord
   end
   
   def tries_valid?
-    if try3.nil? && self.turn != 10
-        if ((self.try1 + self.try2) > 10 )
-            errors.add(:base, 'Enter valid pin entries for each tries')
-        end
-        if ((self.try1 == 10 && self.try2 != 0) || (self.try2 == 10 && self.try1 != 0))
-            errors.add(:base,'In case of strike mark other pin as 0')
-        end
-    elsif try3.nil? && self.turn == 10
-        errors.add(:base, 'Enter valid pin entries for try3')
-    elsif !try3.nil? && self.turn == 10
-        if self.try1 != 10
-            if ((self.try1 +  self.try2 ) > 10 )
+      
+    unless try1.nil? && self.try2.nil?
+        if try3.nil? && self.turn != 10
+            if ((self.try1 + self.try2) > 10 )
                 errors.add(:base, 'Enter valid pin entries for each tries')
-            elsif ((self.try1 +  self.try2 ) < 10 )
-                errors.add(:base, 'Not valid enter try3')
+            end
+            if ((self.try1 == 10 && self.try2 != 0) || (self.try2 == 10 && self.try1 != 0))
+                errors.add(:base,'In case of strike mark other pin as 0')
+            end
+        elsif try3.nil? && self.turn == 10
+            errors.add(:base, 'Enter valid pin entries for try3')
+        elsif !try3.nil? && self.turn == 10
+            if self.try1 != 10
+                if ((self.try1 +  self.try2 ) > 10 )
+                    errors.add(:base, 'Enter valid pin entries for each tries')
+                elsif ((self.try1 +  self.try2 ) < 10 )
+                    errors.add(:base, 'Not valid enter try3')
+                end
             end
         end
+    else
+        errors.add(:base, 'Not valid try1 and try2')
     end
   end
 end
